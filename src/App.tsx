5aa1848fc0fe1f60ded26fea74d7bbf2e8318e86
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-03-14 12:18:21 -0600

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import { Button } from 'antd'
+import { Layout } from 'antd'
+import { Header, Content } from 'antd/es/layout/layout'
+import Sider from 'antd/es/layout/Sider'
+import './index.css'
 
 function App() {
-  return <Button type='primary'>Button</Button>
+  return (
+    <Layout>
+      <Header
+        style={{
+          backgroundColor: 'orange'
+        }}>
+        Header
+      </Header>
+      <Layout>
+        <Sider
+          breakpoint='sm'
+          collapsedWidth='0'
+          trigger={null}
+          width='25%'
+          style={{
+            backgroundColor: 'dodgerblue'
+          }}>
+          Sider
+        </Sider>
+        <Content
+          style={{
+            backgroundColor: 'gold'
+          }}>
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  )
 }
 
 export default App
