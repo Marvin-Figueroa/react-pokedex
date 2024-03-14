aa2db9a244f62c3380d19052e2e2091b5a5f09f2
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2024-03-14 16:49:50 -0600

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,51 @@
-import { Layout } from 'antd'
+import { ConfigProvider, theme, Layout } from 'antd'
 import { Header, Content } from 'antd/es/layout/layout'
 import Sider from 'antd/es/layout/Sider'
 import './index.css'
 import NavBar from './components/NavBar'
+import { useState } from 'react'
+
+const { defaultAlgorithm, darkAlgorithm } = theme
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
+
+  const handleToggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
   return (
-    <Layout>
-      <Header
-        style={{
-          backgroundColor: 'orange'
-        }}>
-        <NavBar />
-      </Header>
+    <ConfigProvider
+      theme={{
+        algorithm: isDarkMode ? darkAlgorithm : defaultAlgorithm
+      }}>
       <Layout>
-        <Sider
-          breakpoint='sm'
-          collapsedWidth='0'
-          trigger={null}
-          width='25%'
-          style={{
-            backgroundColor: 'dodgerblue'
-          }}>
-          Sider
-        </Sider>
-        <Content
-          style={{
-            backgroundColor: 'gold'
-          }}>
-          Content
-        </Content>
+        <Header style={{ backgroundColor: isDarkMode ? '#2b1d11' : 'orange' }}>
+          <NavBar
+            isDarkMode={isDarkMode}
+            onToggleDarkMode={handleToggleDarkMode}
+          />
+        </Header>
+        <Layout>
+          <Sider
+            breakpoint='sm'
+            collapsedWidth='0'
+            trigger={null}
+            width='25%'
+            style={{
+              backgroundColor: 'dodgerblue'
+            }}>
+            Sider
+          </Sider>
+          <Content
+            style={{
+              backgroundColor: 'gold'
+            }}>
+            Content
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,24 @@
-import { Flex, Image, Typography } from 'antd'
+import { Flex, Image, Space, Switch, Typography } from 'antd'
 import Logo from '../assets/pokedex-logo.png'
 
 const { Text } = Typography
 
-const NavBar = () => {
+interface Props {
+  isDarkMode: boolean
+  onToggleDarkMode: () => void
+}
+
+const NavBar = ({ onToggleDarkMode, isDarkMode }: Props) => {
   return (
-    <Flex align='center' gap='large'>
-      <Image width={100} src={Logo} />
-      <Text>NavBar</Text>
+    <Flex justify='space-between' align='center' gap='large'>
+      <Space>
+        <Image preview={false} width={100} src={Logo} />
+        <Text>NavBar</Text>
+      </Space>
+      <Space>
+        <Text>Dark Mode</Text>
+        <Switch checked={isDarkMode} onChange={onToggleDarkMode} />
+      </Space>
     </Flex>
   )
 }
